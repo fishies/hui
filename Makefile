f007a02e78997220a3df7e732c5940c75c35117a
AX <andx@rocketmail.com>
2019-01-26 01:02:44 -0800

    Set up a fullscreen window

--- Makefile
+++ Makefile
@@ -3,7 +3,7 @@ CXXFLAGS = -std=c++17 -Wall -fext-numeric-literals
 LDLIBS = -lsfml-audio -lsfml-graphics -lsfml-window -lsfml-system
 
 hui: main.o
-	$(CXX) $(CXXFLAGS) -o $@ $(LDLIBS) main.o
+	$(CXX) $(CXXFLAGS) main.o -o $@ $(LDLIBS)
 
 main.o: main.cpp
 	$(CXX) $(CXXFLAGS) -o $@ -c main.cpp $(LDLIBS)
--- main.cpp
+++ main.cpp
@@ -1,5 +1,18 @@
+#include <SFML/Graphics.hpp>
+
 int main()
 {
+    sf::RenderWindow window(sf::VideoMode::getFullscreenModes()[0],
+                            "hui", sf::Style::Fullscreen);
+    sf::CircleShape shape(100.f);
+    shape.setFillColor(sf::Color::Black);
+
+    for(;window.isOpen();)
+    {
+        window.clear(sf::Color::White);
+        window.draw(shape);
+        window.display();
+    }
     return 0;
 }
 
